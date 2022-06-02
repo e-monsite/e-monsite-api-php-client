a04c360c483ddf8b179a62218f5e45654bae1d92
Ryan Gille <ryan@e-monsite.com>
2022-06-02 10:25:13 +0200

    first commit

---
+++ curl_generate.sh
@@ -0,0 +1,47 @@
+# Pre-clean
+sudo rm -Rf generated
+sudo rm generated.zip
+
+curl -X POST \
+  https://generator3.swagger.io/api/generate \
+  -H 'Content-Type: application/json' \
+  -H 'cache-control: no-cache' \
+  -d '{
+    "options": {
+      "packageName": "EmonsiteApiPhpClient",
+      "apiPackage": "Api",
+      "invokerPackage": "Emonsite",
+      "composerVendorName": "e-monsite",
+      "composerProjectName": "e-monsite-api-php-client",
+      "modelPackage": "Models",
+      "variableNamingConvention": "camelCase",
+      "packagePath": "GeneratedEmonsiteApiPhpClient",
+      "gitUserId": "e-monsite",
+      "gitRepoId": "e-monsite-api-php-client",
+      "gitRepoBaseURL": "https://github.com/"
+    },
+    "lang": "php",
+    "type": "CLIENT",
+    "codegenVersion": "V3",
+    "specURL": "https://raw.githubusercontent.com/e-monsite/e-monsite-api-php-client/main/e-monsite-oas3.json"
+}' > generated.zip
+
+sudo unzip generated.zip -d generated/
+
+# Remove old files
+sudo rm composer.json
+sudo rm phpunit.xml.dist
+sudo rm README.md
+sudo rm -Rf docs
+sudo rm -Rf lib
+sudo rm -Rf test
+
+# Move file in root directory
+sudo cp -Rf generated/SwaggerClient-php/* .
+
+# Post-clean
+sudo rm -Rf generated
+sudo rm generated.zip
+
+# No use of git_push script, clean it
+sudo rm git_push.sh
